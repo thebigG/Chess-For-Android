b2daa78542bbee57c11d6e362af85b76cad13741
digital-alchemist <lbg65@scarletmail.rutgers.edu>
2019-04-28 20:29:33 -0400

    We have a chessboard, Anton\'s style

--- app/src/main/java/com/android/chess68_android/MainActivity.java
+++ app/src/main/java/com/android/chess68_android/MainActivity.java
@@ -10,9 +10,9 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         //TableLayout = findViewById()
     }
 }
