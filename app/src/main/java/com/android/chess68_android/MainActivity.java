dc5e21b56742fd64b9cd2d22fa42149ab135ad7e
digital-alchemist <lbg65@scarletmail.rutgers.edu>
2019-04-28 20:46:50 -0400

    We have a chessboard, Anton\'s style

--- app/src/main/java/com/android/chess68_android/MainActivity.java
+++ app/src/main/java/com/android/chess68_android/MainActivity.java
@@ -1,18 +1,43 @@
 package com.android.chess68_android;
 
+import android.media.Image;
 import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private TableLayout ChessBoard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+            ChessBoard = (TableLayout) findViewById(R.id.Board);
+            for(int i =0;i<8;i++)
+            {
+                View temp = ChessBoard.getChildAt(i);
+                if(temp instanceof TableRow)
+                {
+                    for(int j =0;j<8;j++)
+                    {
+                        ImageView imageTemp =(ImageView) (( (TableRow)temp).getChildAt(i));
+                        if( imageTemp instanceof ImageView)
+                        {
+                            Log.d("WTF??", imageTemp.toString());
+                        }
+                    }
+                }Log.d("anton",  i+"th piece" + ChessBoard.getChildAt(i).toString() );
+        }
+
         //TableLayout = findViewById()
     }
 }
