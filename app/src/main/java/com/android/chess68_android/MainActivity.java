0ed8ea49820b02fc61663e335ce311967b1b80ab
digital-alchemist <lbg65@scarletmail.rutgers.edu>
2019-04-28 20:22:07 -0400

    We have a chessboard, Antont\'s style

--- app/src/main/java/com/android/chess68_android/MainActivity.java
+++ app/src/main/java/com/android/chess68_android/MainActivity.java
@@ -10,14 +10,9 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        int height = displayMetrics.heightPixels;
-        int width = displayMetrics.widthPixels;
-        Log.d("test", "Screen width:" + Math.floor(width/8));
         //TableLayout = findViewById()
     }
 }
