305334a3c3dc61b5a5f1287443e774de74c7bbcd
digital-alchemist <lbg65@scarletmail.rutgers.edu>
2019-04-28 20:18:09 -0400

    We have a chessboard

--- app/src/main/java/com/android/chess68_android/MainActivity.java
+++ app/src/main/java/com/android/chess68_android/MainActivity.java
@@ -1,7 +1,11 @@
 package com.android.chess68_android;
 
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+        Log.d("test", "Screen width:" + Math.floor(width/8));
+        //TableLayout = findViewById()
     }
 }
